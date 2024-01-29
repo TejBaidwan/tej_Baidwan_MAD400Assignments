e18767cdf9d05384101d8f022943e34fdf6f0685
Tej Baidwan <w0806279@myscc.ca>
2024-01-28 20:32:28 -0500

    Commented out Assignment 2 content and changes project version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Aircraft } from '../models/Aircraft';
+//import { Aircraft } from '../models/Aircraft';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +13,12 @@ import { Aircraft } from '../models/Aircraft';
 
 //Creating array of Aircraft data types to use
 export class AppComponent {
-  title = 'Airplane-List';
-  aircraft = [
-    new Aircraft("747-400", 416, 4, true, "February 22nd, 2024"),
-    new Aircraft("DC-10", 270, 3),
-    new Aircraft("A380", 525, 4, true)
-  ]
+  title = '';
+  //aircraft = [
+    //new Aircraft("747-400", 416, 4, true, "February 22nd, 2024"),
+    //new Aircraft("DC-10", 270, 3),
+    //new Aircraft("A380", 525, 4, true)
+  //]
 
   //This empty array init can be used to test the ngif directive in app.component.html
   //aircraft: Aircraft[] = [];
--- src/models/Aircraft.ts
+++ src/models/Aircraft.ts
@@ -1,13 +1,16 @@
 /**
  * Class representing an Aircraft
  */
-export class Aircraft {
-  constructor(
-    public planeModel: string,
-    public capacity: number,
-    public engines: number,
-    public flyingStatus: boolean = false,
-    public nextMaintenance?: string
-  ) {
-  }
-}
+
+/**
+ * export class Aircraft {
+ *   constructor(
+ *     public planeModel: string,
+ *     public capacity: number,
+ *     public engines: number,
+ *     public flyingStatus: boolean = false,
+ *     public nextMaintenance?: string
+ *   ) {
+ *   }
+ * }
+ */
