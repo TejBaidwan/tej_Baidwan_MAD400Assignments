a82b1a7079cc9495d43c2848263eca7b55b88be2
Tej Baidwan <w0806279@myscc.ca>
2024-02-11 00:07:27 -0500

    Modified package.json version and moved IAircraft array into new file

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -19,38 +19,38 @@ import {NgClass, NgForOf} from "@angular/common";
  * Creating an array of IAircraft objects
  */
 export class ContentListComponent {
-  airplaneList: IAircraft[] = [
-    {
-      name: 'Boeing 747',
-      imgSrc: 'assets/b747.jpg',
-      description: 'The Boeing 747 is a commonly used passenger plane with over 300 seats.',
-      author: 'Image by WikimediaImages from Pixabay',
-      type: 'Commercial',
-      tags: ['Boeing', 'Airplane', 'Commercial'],
-    },
-    {
-      name: 'F-35',
-      imgSrc: 'assets/f35.jpg',
-      description: 'The F-35 is a highly-advanced US Military fighter jet.',
-      author: 'Image by WikiImages from Pixabay',
-      type: 'Military',
-      tags: ['F-35', 'Fighter Jet', 'Military'],
-    },
-    {
-      name: 'Airbus A380',
-      imgSrc: 'assets/a380.jpg',
-      description: 'The Airbus A380 is the biggest passenger airliner with over 500 seats..',
-      author: 'Image by Pit Karges from Pixabay',
-      type: 'Commercial',
-      tags: ['Airbus', 'A380', 'Commercial'],
-    },
-    {
-      name: 'Concorde',
-      imgSrc: 'assets/concorde.jpg',
-      description: 'The Concorde was a super-sonic passenger aircraft created in France.',
-      author: 'Image by Chris Jones from Pixabay',
-      type: 'Commercial',
-      tags: ['Concorde', 'France', 'Supersonic Flight'],
-    }
-  ]
+  // airplaneList: IAircraft[] = [
+  //   {
+  //     name: 'Boeing 747',
+  //     imgSrc: 'assets/b747.jpg',
+  //     description: 'The Boeing 747 is a commonly used passenger plane with over 300 seats.',
+  //     author: 'Image by WikimediaImages from Pixabay',
+  //     type: 'Commercial',
+  //     tags: ['Boeing', 'Airplane', 'Commercial'],
+  //   },
+  //   {
+  //     name: 'F-35',
+  //     imgSrc: 'assets/f35.jpg',
+  //     description: 'The F-35 is a highly-advanced US Military fighter jet.',
+  //     author: 'Image by WikiImages from Pixabay',
+  //     type: 'Military',
+  //     tags: ['F-35', 'Fighter Jet', 'Military'],
+  //   },
+  //   {
+  //     name: 'Airbus A380',
+  //     imgSrc: 'assets/a380.jpg',
+  //     description: 'The Airbus A380 is the biggest passenger airliner with over 500 seats..',
+  //     author: 'Image by Pit Karges from Pixabay',
+  //     type: 'Commercial',
+  //     tags: ['Airbus', 'A380', 'Commercial'],
+  //   },
+  //   {
+  //     name: 'Concorde',
+  //     imgSrc: 'assets/concorde.jpg',
+  //     description: 'The Concorde was a super-sonic passenger aircraft created in France.',
+  //     author: 'Image by Chris Jones from Pixabay',
+  //     type: 'Commercial',
+  //     tags: ['Concorde', 'France', 'Supersonic Flight'],
+  //   }
+  // ]
 }
