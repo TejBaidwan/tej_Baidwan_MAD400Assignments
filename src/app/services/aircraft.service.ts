188836657bee8813b1a1c79b06be99560e7036d8
Tej Baidwan <w0806279@myscc.ca>
2024-02-11 00:30:41 -0500

    Created method to return observable of IAircraft array in data folder

--- src/app/services/aircraft.service.ts
+++ src/app/services/aircraft.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { AIRCRAFTLIST } from '../content-list/data/IAircraftContent';
+import {IAircraft} from "../../models/IAircraft";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,8 @@ import { Injectable } from '@angular/core';
 export class AircraftService {
 
   constructor() { }
+
+  fetchAircraft(): Observable<IAircraft[]> {
+    return of(AIRCRAFTLIST);
+  }
 }
